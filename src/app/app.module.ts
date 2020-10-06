843814c2cd3802838b51473726cb839afba420d8
Billson Chew <billson.bs.chew@tiongnam.local>
2020-10-06 13:58:44 +0800

    Implement https://v6.angular.io/tutorial/toh-pt2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+//  Billson@20201006_pm015059 : FormsModule symbol import
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //  Billson@20201006_pm015117 : @NgModule imports
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
