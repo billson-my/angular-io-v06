e79002fa78a312affa2c9776917b2df8842fd64a
Billson Chew <billson.bs.chew@tiongnam.local>
2020-10-06 14:55:07 +0800

    Implement - https://v6.angular.io/tutorial/toh-pt3#make-the-herodetailcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 //  Billson@20201006_pm015059 : FormsModule symbol import
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
